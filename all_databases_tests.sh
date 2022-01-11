1218e666289cd9f74cb881cd7405a01d28fa8388
ibelousov <i.a.belousov.88@gmail.com>
2022-01-11 21:09:32 +0400

    fix travis config

--- all_databases_tests.sh
+++
@@ -1,8 +0,0 @@
-echo "Start docker compose"
-docker-compose up -d
-tput setaf 3; echo "SQLite testing"; tput sgr0
-vendor/bin/phpunit
-tput setaf 3; echo "POSTGRESQL testing"; tput sgr0
-vendor/bin/phpunit --configuration phpunit.pgsql.xml
-echo "End"
-docker-compose down
