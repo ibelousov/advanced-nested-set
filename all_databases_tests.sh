ce6fb597d56f5585554609c11932f9dd2526780a
ibelousov <i.a.belousov.88@gmail.com>
2022-01-11 17:51:07 +0400

    fix phpunit config

--- all_databases_tests.sh
+++ all_databases_tests.sh
@@ -3,6 +3,6 @@ docker-compose up -d
 tput setaf 3; echo "SQLite testing"; tput sgr0
 vendor/bin/phpunit
 tput setaf 3; echo "POSTGRESQL testing"; tput sgr0
-vendor/bin/phpunit --configuration phpunit.pgsql.xml.dist
+vendor/bin/phpunit --configuration phpunit.pgsql.xml
 echo "End"
 docker-compose down
