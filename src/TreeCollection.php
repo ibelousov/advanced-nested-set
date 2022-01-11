70a183bd4e1f750ea84120f5cc3634745f15b191
ibelousov <i.a.belousov.88@gmail.com>
2022-01-11 17:54:21 +0400

    add TreeCollection - special collection that supports converting to tree

---
+++ src/TreeCollection.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace Ibelousov\AdvancedNestedSet;
+
+
+class TreeCollection extends \Illuminate\Database\Eloquent\Collection
+{
+    protected $isTree = false;
+
+    public function __construct($items = [], $isTree = false)
+    {
+        $this->isTree = $isTree;
+
+        parent::__construct($items);
+    }
+
+    public function toTree($root = null)
+    {
+        $tree = new self([], true);
+
+        foreach($this as $item)
+            if($item->parent_id == $root) {
+                $item->setAttribute('children', $this->toTree($item->id));
+                $tree->push($item);
+            }
+
+        return $tree;
+    }
+
+    public function toArray()
+    {
+        if(!$this->isTree)
+            return $this->toArray();
+
+        return $this->toTreeArray();
+    }
+
+    protected function toTreeArray()
+    {
+        $array = [];
+
+        foreach($this as $item) {
+            $item = $item->toArray();
+            $item['children'] = $item['children']->toArray();
+            $array[] = $item;
+        }
+
+        return $array;
+    }
+}
